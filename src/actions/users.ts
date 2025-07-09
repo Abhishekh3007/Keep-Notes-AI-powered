91026af4a6e28036dee4551efda3068c0c51161f
Abhishekh3007 <waliabhishek120@gmail.com>
2025-07-09 23:09:37 +0530

    ui part half

---
+++ src/actions/users.ts
@@ -0,0 +1,58 @@
+"use server";
+import { createClient } from "@/auth/server";
+import { handleError } from "@/lib/utils";
+
+import { Hand } from "lucide-react";
+
+export const loginAction = async (email: string, password: string) => {
+  try {
+    const { auth } = await createClient();
+    const { error } = await auth.signInWithPassword({ email, password });
+    if (error) throw error;
+
+    // Verify that the user is actually logged in after sign-in
+    const userObject = await auth.getUser();
+    if (!userObject || !userObject.data.user) {
+      throw new Error("Failed to authenticate user");
+    }
+
+    return { errorMessage: null };
+  } catch (error) {
+    return handleError(error);
+  }
+};
+export const logOutAction = async () => {
+  try {
+    const { auth } = await createClient();
+
+    const { error } = await auth.signOut();
+    if (error) throw error;
+
+    return { errorMessage: null };
+  } catch (error) {
+    return handleError(error);
+  }
+};
+
+export const signUpAction = async (email: string, password: string) => {
+  try {
+    const { auth } = await createClient();
+    const { data, error } = await auth.signUp({ email, password });
+    if (error) throw error;
+
+    const userId = data.user?.id;
+    if (!userId) throw new Error("Failed to get user ID after signup");
+
+    // Add user to database here
+    // await prisma.user.create({
+    //   data: {
+    //     id: userId,
+    //     email,
+    //   },
+    // });
+
+    return { errorMessage: null };
+  } catch (error) {
+    return handleError(error);
+  }
+};
---
+++ src/components/ui/header.tsx
@@ -0,0 +1,52 @@
+import Link from 'next/link'
+import React from 'react'
+import Image from 'next/image';
+import { shadow } from '@/app/styles/utils';
+import {Button} from '@/components/ui/button';
+import {ModeToggle} from '@/components/ui/DarkModeToggle'
+import Logoutbutton from '@/components/ui/logoutbutton';
+import { getUser } from '@/auth/server';
+
+  async function Header() {
+    const user=await getUser();
+  return (
+    
+    <header
+      className="bg-popover relative flex h-24 w-full items-center justify-between px-3 sm:px-8"
+      style={{
+        boxShadow: shadow,
+      }}
+    >
+     
+
+      <Link className="flex items-end gap-2" href="/">
+        <Image
+          src="/luffy.png"
+          height={60}
+          width={60}
+          alt="logo"
+          className="rounded-full"
+          priority
+        />
+
+        <h1 className="flex flex-col pb-1 text-2xl font-semibold leading-6">
+          KEEP <span>Notes</span>
+        </h1>
+      </Link>
+      <div className="flex  gap-4">
+        {user ? (
+           <Logoutbutton/>
+        ) : (
+           <>
+            <Button className="hidden sm:block" asChild><Link href="/sign-up">SignUp</Link></Button>
+           <Button  asChild variant='outline'><Link href="/Login">Login</Link></Button>
+            
+           </>
+           
+        )}
+      </div>
+      <ModeToggle />
+    </header>
+  )
+}
+export default Header;
